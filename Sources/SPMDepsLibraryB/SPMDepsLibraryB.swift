69695072fa2f5a68aa2100b9bc8ff06709f08ca1
Nikolay Valasatau <mikalai.valasatau@mapbox.com>
2022-01-21 17:39:22 +0300

    v10.0.0

--- Sources/SPMDepsLibraryB/SPMDepsLibraryB.swift
+++ Sources/SPMDepsLibraryB/SPMDepsLibraryB.swift
@@ -1,4 +1,9 @@
+import SPMDepsLibraryA
+
 public struct SPMDepsLibraryB {
+    public static let versionA = SPMDepsLibraryA.version
+    public static let versionB = "10.0.0"
+
     public private(set) var text = "Hello, World!"
 
     public init() {
